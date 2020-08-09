b15ff498a30fda25db34f12f20cae2b58ca2ad79
Alexander <kobzon3000bmx@mail.ru>
2020-08-09 09:22:55 +0400

    add enter form validation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './styles/validation.css';
-import { validateRegister } from './js/validation';
+import { validateRegister, validateEnter } from './js/validation';
 import { switching } from './js/tabs';
 
 const tabsButton = document.querySelectorAll('.js-tab-button');
@@ -9,8 +9,13 @@ const formRegistrationOptions = {
 	formSelector: document.querySelector('.js-form-registration')
 };
 
+const formLoginOptions = {
+	formSelector: document.querySelector('.js-form-login')
+};
+
 if (tabsButton && formContent) {
 	switching(tabsButton, formContent)
 }
 
 validateRegister(formRegistrationOptions);
+validateEnter(formLoginOptions);
--- src/js/validation.js
+++ src/js/validation.js
@@ -128,3 +128,105 @@ export const validateRegister = ({ formSelector }) => {
 		alert(JSON.stringify(form, null, 2))
 	})
 };
+
+export const validateEnter = ({ formSelector }) => {
+	const buttonLogin = formSelector.querySelector('.js-button-login');
+
+	const errorContainers = [...formSelector.querySelectorAll('.js-error-login')];
+
+	const errors = {
+		email: '',
+		password: ''
+	};
+	
+
+	const form = {
+		email: '',
+		password: ''
+	};
+
+	const isValid = {
+		email: false,
+		password: false
+	}
+
+	const initalColor = '#343436';
+	const errorColor = '#fa0019';
+
+	const setError = (inputItem) => {
+		let input = inputItem;
+		input.style.borderColor = errorColor
+	};
+
+	const setValid = (inputItem) => {
+		let input = inputItem;
+		input.style.borderColor = initalColor
+	};
+
+	const errorMessage = (inputName, containers) => {
+		containers.map((container) => {
+			if (inputName === container.dataset.name) {
+				container.textContent = errors[inputName]
+			}
+		})
+	};
+
+	const validateEmail = (value, input) => {
+		if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value)) {
+			errors[input.name] = 'Неверный формат'
+			isValid[input.name] = false;
+			return false;
+		}
+		errors[input.name] = ''
+		isValid[input.name] = true;
+		return true;
+	};
+
+	const validatePassword = (value, input) => {
+		if (value.length < 6) {
+			errors[input.name] = 'Не менее шести символов'
+			isValid[input.name] = false;
+			return true;
+		}
+		errors[input.name] = ''
+		isValid[input.name] = true;
+		return false;
+	};
+
+	const validate = (type, input) => {
+		switch (type) {
+			case 'email': {
+				if (validateEmail(input.value, input)) {
+					setValid(input)
+					return;
+				}
+				setError(input)
+				break;
+			}
+			case 'password': {
+				if (validatePassword(input.value, input)) {
+					setError(input)
+					return;
+				}
+				setValid(input)
+				break;
+			}
+		}
+	};
+
+	formSelector.addEventListener('input', (event) => {
+		const target = event.target;
+		form[target.name] = target.value
+		validate(target.type, target);
+		errorMessage(target.name, errorContainers)
+	
+		const valid = (isValid.password && isValid.email);
+
+		valid ? buttonLogin.removeAttribute('disabled') : buttonLogin.setAttribute('disabled', true);
+	})
+
+	buttonLogin.addEventListener('click', (e) => {
+		e.preventDefault()
+		alert(JSON.stringify(form, null, 2))
+	})
+};
